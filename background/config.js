506876288f496dffa8faa54e15b415f7fc649fcb
ayub <sir.kokabi@gmail.com>
2025-12-11 11:28:07 +0330

    remove unstable Nitter instances

--- background/config.js
+++ background/config.js
@@ -1,13 +1,6 @@
 export const NITTER_INSTANCES = [
-  "https://nitter.net",
-  "https://nitter.poast.org",
-  "https://nitter.tiekoetter.com",
-  "https://nitter.privacyredirect.com",
-  "https://nitter.space",
-  "https://xcancel.com",
-  "https://lightbrd.com",
-  "https://nuku.trabun.org",
-  "https://nitter.catsarch.com"
+"https://nitter.net",
+"https://nitter.privacyredirect.com",
 ];
 
 export const TIMEOUT_MS = 8000;
